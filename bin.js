fccf10f5fc6f583a66871609708e4dfb234154c4
Jakub Szwacz <jakub@szwacz.com>
2016-12-12 21:50:23 +0100

    Added command line interface

--- bin.js
+++ bin.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+const program = require('commander');
 const api = require('./index.js');
 
-api.start();
+program
+  .version(require('./package.json').version)
+  .option('-s, --start', 'start release process')
+  .parse(process.argv);
+
+if (program.start) {
+  api.start();
+}
