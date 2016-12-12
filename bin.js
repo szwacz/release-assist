e2e4867363c4a77c0066895bb339b5f30728ea59
Jakub Szwacz <jakub@szwacz.com>
2016-12-12 22:57:37 +0100

    Release process fully implemented

--- bin.js
+++ bin.js
@@ -6,8 +6,11 @@ const api = require('./index.js');
 program
   .version(require('./package.json').version)
   .option('-s, --start', 'start release process')
+  .option('-f, --finish', 'finish release process')
   .parse(process.argv);
 
 if (program.start) {
   api.start();
+} else if (program.finish) {
+  api.finish();
 }
